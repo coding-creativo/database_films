7a81ac36674a1c336385c975b4da2793f728a638
Cristina <codingcreativo@gmail.com>
2024-06-07 12:20:52 +0200

    database-films

---
+++ database/migrations/2024_06_07_092533_create_films_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('director_id');
+            $table->integer('year');
+            $table->timestamps();
+
+            $table->foreign('director_id')->references('id')->on('directors')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('films');
+    }
+};
